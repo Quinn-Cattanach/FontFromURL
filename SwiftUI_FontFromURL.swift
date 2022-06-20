7a60dbebd4f6f4fc9136f3f1b1957657a34f731a
Quinn-Cattanach <85038165+Quinn-Cattanach@users.noreply.github.com>
2022-06-20 18:10:30 -0400

    Create SwiftUI_FontFromURL.swift

---
+++ SwiftUI_FontFromURL.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+#if os(macOS)
+import Cocoa
+typealias PlatformFont = NSFont
+#elseif os(iOS)
+import UIKit
+typealias PlatformFont = UIFont
+#endif
+
+extension Font {
+    init?(_ absoluteURL: URL, size: CGFloat = 12.0) {
+        if let cfbr = CFBridgingRetain(absoluteURL) {
+            
+            let cfURL = cfbr as! CFURL
+            
+            if let dataProvider = CGDataProvider(url: cfURL),
+               let cgFont = CGFont(dataProvider) {
+                let font: PlatformFont = CTFontCreateWithGraphicsFont(cgFont, 24.0, nil, nil)
+                self = Font(font as CTFont)
+            }
+        }
+        return nil
+    }
+    init?(_ fontName: String,
+            fileExtension: String? = nil,
+            size: CGFloat = 12.0) {
+        if let url = Bundle.main.url(forResource: fontName, 
+                                     withExtension: fileExtension),
+                                     let font = Self.init(url, size: size) {
+            self = font
+        } else {
+            return nil
+        }
+    }
+}
